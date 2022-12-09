c443030a7a3892c922f1da14b7d48e26c9f2e0c5
Mary Banzon <maryelizabeth.banzon@gmail.com>
2022-12-08 23:20:39 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+var rounds;
+var player;
+var opponent;
+var roundnum;
+var randomNum;
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+roundnum = prompt("How many rounds would you like to play?");
+rounds = parseInt(roundnum, 10);
+
+for (i = 0; i < rounds; i++) {
+    player = prompt("Rock, paper, or scissors?");
+
+    if (player == "r" || player == "rock" || player == "Rock") {
+        player = "Rock";
+    } else if (player == "p" || player == "paper" || player == "Paper") {
+        player = "Paper";
+    } else if (player == "s" || player == "scissors" || player == "Scissors") {
+        player = "Scissors";
+    } else {
+        alert("Invalid input. Please enter a valid choice.");
+    }
+
+    randomNum = Math.floor(Math.random() * 3);
+
+    // alert(randomNum);
+
+    if (randomNum == 0) {
+        opponent = "Rock";
+    } else if (randomNum == 1) {
+        opponent = "Paper";
+    } else if (randomNum == 2) {
+        opponent = "Scissors";
+    }
+
+    // alert(opponent);
+
+    if (player == "Rock" && opponent == "Scissors") {
+        wins = wins + 1;
+        alert("You won! " + wins);
+    } else if (player == "Rock" && opponent == "Paper") {
+        losses = losses + 1;
+        alert("You lost! " + losses);
+    } else if (player == "Paper" && opponent == "Rock") {
+        wins = wins + 1;
+        alert("You won! " + wins)
+    } else if (player == "Paper" && opponent == "Scissors") {
+        losses = losses + 1;
+        alert("You lost! " + losses);
+    } else if (player == "Scissors" && opponent == "Paper") {
+        wins = wins + 1;
+        alert("You won! " + wins);
+    } else if (player == "Scissors" && opponent == "Rock") {
+        losses = losses + 1;
+        alert("You lost! " + losses);
+    } else if (player == opponent) {
+        ties = ties + 1;
+        alert("It's a tie! " + ties);
+    }
+}
+
+alert ("Your total score is " + wins + " wins, " + losses + " losses, and " + ties + " ties. Thanks for playing!");
